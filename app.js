2130c0e4397fb932efec0e891a70d727fac99b43
Brent <brentgreeff@gmail.com>
2019-05-09 19:16:28 +0200

    Basic user routes framework.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const express = require('express');
 
 const app = express();
+
+app.use( morgan('dev') );
+app.use( bodyParser.json() );
+
+app.use( '/users', require('./routes/users') );
+
 const port = process.env.PORT || 3000;
 app.listen(port);
-console.log(`Server on ${port}`);
+console.log(`Server UP on ${port}`);

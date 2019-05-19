dcd2023fd818565f7d21dd985b139f5d22d8a44f
Brent <brentgreeff@gmail.com>
2019-05-19 21:10:46 +0200

    Return an error if email already taken.

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,19 @@
 const User = require('../models/user');
 
+const findUser = async ({email}) => {
+  return await User.findOne({ email })
+}
+
 module.exports = {
 
   signUp: async (req, res, next) => {
     const { email, password } = req.value.body;
+
+    if ( findUser({ email }) ) {
+      res.status(403).json({ error: `${email} already taken` })
+      return;
+    }
+
     const newUser = new User({
       email,
       password,

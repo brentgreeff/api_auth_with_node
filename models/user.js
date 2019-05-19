f3c15fa2a8341fb473754e76456dba9dc5cd8a16
Brent <brentgreeff@gmail.com>
2019-05-19 21:41:42 +0200

    Add mongoose validations on user.

--- models/user.js
+++ models/user.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: String,
-  password: String
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
 });
 
 const User = mongoose.model('user', userSchema);
